2e1cade1c3b6cc1836ed9406276f97d16e846cc8
Dustin Lucien <dustin.lucien@gmail.com>
2011-05-05 15:29:59 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Einfluss::Application.config.secret_token = 'c97d47f9814b636f3a1170f5fde585688614dbb9961ba5be50a489cbfb0d7226efb42b26b3963212d466bdf29e86798f1cae46dd732d58282377a3d0af8499ae'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Einfluss::Application.config.session_store :cookie_store, :key => '_einfluss_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Einfluss::Application.config.session_store :active_record_store
